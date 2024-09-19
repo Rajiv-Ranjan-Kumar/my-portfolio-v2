013c2f04584a0617073f9ff273dd5f22c0f42945
Rajiv-Ranjan-Kumar <rrks.195@gmail.com>
2024-09-19 08:58:52 +0530

    some navigation isses fixed

--- frontEnd/portfolio/src/components/header.tsx
+++ frontEnd/portfolio/src/components/header.tsx
@@ -16,12 +16,12 @@ export default function Header() {
 
       {/* Navigation links with conditional rendering for menu visibility */}
       <nav className={isMenuButton ? `${styles.nav} ${styles.navDisplay} ${styles.open}` : isMenuButtonClicked? `${ styles.nav } ${styles.navDisplay} ${ styles.close }`: styles.nav} >
-        <a href='#home'>Home</a>
-        <a href='#about'>About</a>
-        <a href='#skills'>Skills</a>
-        <a href='#services'>Services</a>
-        <a href='#projects'>Projects</a>
-        <a href='#contact'>Contact</a>
+        <a href='#home' onClick={() => document.getElementById('home')?.scrollIntoView({ behavior: 'smooth' })}>Home</a>
+        <a href='#about' onClick={() => document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' })}>About</a>
+        <a href='#skills' onClick={() => document.getElementById('skills')?.scrollIntoView({ behavior: 'smooth' })}>Skills</a>
+        <a href='#services' onClick={() => document.getElementById('services')?.scrollIntoView({ behavior: 'smooth' })}>Services</a>
+        <a href='#projects' onClick={() => document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' })}>Projects</a>
+        <a href='#contact' onClick={() => document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' })}>Contact</a>
       </nav>
 
       {/* Menu button (hamburger icon) for mobile view, with toggle effect */}
