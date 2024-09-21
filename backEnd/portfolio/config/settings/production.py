5f72d8e1980b18d32aa7bf11e1219ce2dabf2684
Rajiv-Ranjan-Kumar <rrks.195@gmail.com>
2024-09-22 02:14:13 +0530

    database releted settings done.

--- backEnd/portfolio/config/settings/production.py
+++ backEnd/portfolio/config/settings/production.py
@@ -35,7 +35,7 @@ MIDDLEWARE = MIDDLEWARE + [
 # Database settings for production (PostgreSQL)
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv('DB_NAME'),
         "USER": os.getenv('DB_USER'),
         "PASSWORD": os.getenv('DB_PASSWORD'),
