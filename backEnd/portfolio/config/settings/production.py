081c061da3987bfb082bc7cfff56c14c4bd062bf
Rajiv-Ranjan-Kumar <rrks.195@gmail.com>
2024-09-22 12:13:07 +0530

    vercel settings update

--- backEnd/portfolio/config/settings/production.py
+++ backEnd/portfolio/config/settings/production.py
@@ -53,7 +53,7 @@ DATABASES = {
 # Static and media files for production
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
