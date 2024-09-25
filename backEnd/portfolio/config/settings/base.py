51087bfc4cdad1aaef226e6c0eb5c10f1ba6ec88
Rajiv-Ranjan-Kumar <rrks.195@gmail.com>
2024-09-26 02:35:11 +0530

    404 esues dones.

--- backEnd/portfolio/config/settings/base.py
+++ backEnd/portfolio/config/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -115,8 +116,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Media files (if any)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- backEnd/portfolio/config/settings/production.py
+++ backEnd/portfolio/config/settings/production.py
@@ -14,7 +14,7 @@ if not SECRET_KEY:
     raise ValueError(f"{ RED }The SECRET_KEY setting must not be empty.{ RESET }")
 
 
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = [config('ALLOWED_HOSTS'),'*']
@@ -54,3 +54,6 @@ DATABASES = {
 SUPER_USER_NAME = config('SUPER_USER_NAME')
 SUPER_USER_EMAIL = config('SUPER_USER_EMAIL')
 SUPER_USER_PASSWORD = config('SUPER_USER_PASSWORD')
+
+
+CORS_ORIGIN_ALLOW_ALL = True
